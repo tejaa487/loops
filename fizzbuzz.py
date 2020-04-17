29d6e92d45450dfce16acc477f5ce0ac34fb097c
tejaa487 <63533249+tejaa487@users.noreply.github.com>
2020-04-17 07:09:40 +0530

    simple game

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+def fizzbuzz(r):
+    for i in range(1,r):
+        if i%3==0 and i%5==0:
+            print(str(i)+"fizzbuzz")
+        else:
+            if i%3==0:
+                print(str(i)+"fizz")
+            else:
+                if i%5==0:
+                    print(str(i)+"buzz")
+                else:
+                    print(str(i))
+
+
+
+fizzbuzz(35)
