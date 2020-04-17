dfa24d0ad19f1913f94f88061b95b5191cb4088b
tejaa487 <63533249+tejaa487@users.noreply.github.com>
2020-04-17 07:05:16 +0530

    finding greatest among 3 nums

---
+++ greatest among three nums.py
@@ -0,0 +1,10 @@
+a=int(input("enter 1st value"))
+b=int(input("enter 2nd value"))
+c=int(input("enter 3rd value"))
+if a>=b and a>=c:
+    print(a,"is greater")
+elif b>=a and b>=c:
+    print(b,"is greater")
+else:
+    print(c,"is greater")
+    
